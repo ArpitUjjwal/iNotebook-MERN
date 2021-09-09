ba511b7cda3d9fbf3dbe4828128881b8101dedcd
Arpit <arpitujjwal1@gmail.com>
2021-09-09 13:55:49 +0530

    authtoken

--- backend/models/User.js
+++ backend/models/User.js
@@ -19,6 +19,7 @@ const UserSchema = new Schema({
         type: Date,
         default: Date.now
     },
-  });
+});
 
-  module.exports = mongoose.model('user', UserSchema)
+const User = mongoose.model('user', UserSchema)
+module.exports = mongoose.model('user', UserSchema)
